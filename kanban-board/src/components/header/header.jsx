b1231ad72765a4ea16abc74699f8a26cb4376358
Ilia Klimov <ilklmv21@gmail.com>
2023-12-15 21:08:43 +0300

    Edited header

--- kanban-board/src/components/header/header.jsx
+++ kanban-board/src/components/header/header.jsx
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import './header.css';
 
 const Header = () => {
+    const [ menuVisible, setMenuVisible] = useState(false);
+
+    // eslint-disable-next-line no-unused-vars
+    const handleArrowClick = () => {
+        setMenuVisible(!menuVisible);
+    }
+
     return (
         <header className="header">
-            <div className="header__title">Kanban board</div>
+            <div className="header__title">Awesome Kanban board</div>
             <div className="header__user-menu">
                 <div className="user-avatar">
                     <img src="avatarUrl" alt="User Avatar" />
                 </div>
-                <div className="arow-down">
+                <div className="arrow-down">
                     <svg
                         viewBox="0 -4.5 20 20"
                         version="1.1"
                         xmlns="http://www.w3.org/2000/svg"
                         xmlnsXlink="http://www.w3.org/1999/xlink"
-                        fill="#000000"
+                        fill="#ffffff"
                     >
                         <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
                         <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
@@ -24,7 +31,7 @@ const Header = () => {
                             <desc>Created with Sketch.</desc>
                             <defs></defs>
                             <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-                                <g id="Dribbble-Light-Preview" transform="translate(-220.000000, -6684.000000)" fill="#000000">
+                                <g id="Dribbble-Light-Preview" transform="translate(-220.000000, -6684.000000)" fill="#ffffff">
                                     <g id="icons" transform="translate(56.000000, 160.000000)">
                                         <path
                                             d="M164.292308,6524.36583 L164.292308,6524.36583 C163.902564,6524.77071 163.902564,6525.42619 164.292308,6525.83004 L172.555873,6534.39267 C173.33636,6535.20244 174.602528,6535.20244 175.383014,6534.39267 L183.70754,6525.76791 C184.093286,6525.36716 184.098283,6524.71997 183.717533,6524.31405 C183.328789,6523.89985 182.68821,6523.89467 182.29347,6524.30266 L174.676479,6532.19636 C174.285736,6532.60124 173.653152,6532.60124 173.262409,6532.19636 L165.705379,6524.36583 C165.315635,6523.96094 164.683051,6523.96094 164.292308,6524.36583"
@@ -36,6 +43,12 @@ const Header = () => {
                         </g>
                     </svg>
                 </div>
+                {menuVisible && (
+                    <div className="dropdown-menu">
+                        <a href="/profile">Profile</a>
+                        <a href="/logout">Log Out</a>
+                    </div>
+                )}
             </div>
         </header>
     );
